413332a95154dd9b0fde81250cf452575730afeb
AbdallahMohamed79 <113598906+AbdallahMohamed79@users.noreply.github.com>
2022-10-13 21:55:17 +0200

    Add files via upload
    Upload phone

---
+++ index.js
@@ -0,0 +1,12 @@
+let img = document.querySelector('.img')
+let container = document.querySelector('.container') 
+let h2 = document.querySelector('.h2') 
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    container.style.background = color;
+}
+function col(colorh){
+    h2.style.color = color;
+}
